5bd36c4812c5475b00bd642c45995ada3d7da3f2
Yifu <yifu3@ualberta.ca>
2020-06-14 19:49:49 +0800

    update from mac

--- leetcode/Tree/116. 给每个node创造一个指向右的指针.py
+++ leetcode/Tree/116. 给每个node创造一个指向右的指针.py
@@ -8,30 +8,35 @@ class Node(object):
         self.next = next
 """
 
-
 class Solution(object):
     def connect(self, root):
         """
         :type root: Node
         :rtype: Node
         """
+
         level_start = root
 
-        while level_start != None:
+        while level_start:
             cur = level_start
+
             while cur:
+                # 处理cur的左节点，让它指向cur的右节点
                 if cur.left:
                     cur.left.next = cur.right
+
+                # 处理cur的右节点，然它指向cur同层的右边节点
+                # 所以我们这里还要判断 cur同层的右边节点存不存在
                 if cur.right and cur.next != None:
                     cur.right.next = cur.next.left
 
+                # 处理完同层最左边的，开始向同层的右边走，处理下一个
                 cur = cur.next
+
+            # 当同层的处理完了，就去下一层
             level_start = level_start.left
 
         return root
 
-"""
-https://www.youtube.com/watch?v=_RMIQ3hDTQs
-答案：
-
-"""
+# Time: O(n), Space: O(1)
+# https://algocasts.io/episodes/Q2preyGz
