8f38738a71d52c6ac427e1cbff0dd2f0e4e08c0f
Yifu <yifu3@ualberta.ca>
2021-12-06 22:13:36 +0800

    416, 494update

--- leetcode/DP/背包问题/416. 分割等和子集(恰好取到背包容量).py
+++ leetcode/DP/背包问题/416. 分割等和子集(恰好取到背包容量).py
@@ -7,17 +7,18 @@ class Solution(object):
 
         total = sum(nums)
 
-        # 不能正好被分割， 所以是False
+        # 总和不能刚好被平分成两份，所以不可能构成
         if total % 2 == 1:
             return False
 
-        total = total // 2
+        # 每一份数应该是1/2个total
+        total //= 2
 
         dp = [[False for j in range(total + 1)] for i in range(len(nums))]
 
-        # 第 0 行，第 1 个数只能让容积为它自己的背包恰好装满
-        if nums[0] <= total:
-            dp[0][nums[0]] = True
+        # base case, 使用前i个数，合为0，那么就是所有的数都不去选择
+        for i in range(len(nums)):
+            dp[i][0] = True
 
         for i in range(1, len(nums)):
             for j in range(1, total + 1):
@@ -32,6 +33,9 @@ class Solution(object):
                 # 加上当前这个数，能不能填满背包
                 if nums[i] < j:
                     dp[i][j] = dp[i - 1][j] or dp[i - 1][j - nums[i]]
+                # 假如nums[i] 大于背包容量，那么就不应该考虑把这个数加进背包
+                if nums[i] > j:
+                    dp[i][j] = dp[i - 1][j]
 
         return dp[-1][total]
 
@@ -39,15 +43,16 @@ class Solution(object):
 """
 时间复杂度：O(NC)：这里 N是数组元素的个数, C 是数组元素的和的一半。
 空间复杂度：O(NC)
-https://leetcode-cn.com/problems/partition-equal-subset-sum/solution/0-1-bei-bao-wen-ti-xiang-jie-zhen-dui-ben-ti-de-yo/
+https://leetcode-cn.com/problems/partition-equal-subset-sum/solution/fen-ge-deng-he-zi-ji-by-leetcode-solution/
 
 如何理解本题的状态转移方程：
 dp[i][j]表示从数组的 [0, i] 这个子区间内挑选一些正整数，每个数只能用一次，使得这些数的和恰好等于 j
-dp[i][j]表示，对于前 i 个物品，当前背包的容量为j时，若 x 为 true，则说明可以恰好将背包装满，若 x 为 false，则说明不能恰好将背包装满。
-比如说，如果 dp[4][9] = true，其含义为：对于容量为 9 的背包，若只是用前 4 个物品，可以有一种方法把背包恰好装满。
+dp[i][j]表示，对于前 i 个物品，当前背包的容量为j时
+    若 dp[i][j] 为 true，则说明有一种选择方法的和为j
+    若 dp[i][j] 为 false，则说明没有有一种选择方法的和为j
+比如说，如果 dp[4][9] = true，其含义为：对于容量为 9 的背包，若只是用前 4 个物品，至少有一种可能能刚好装到9。
 
 状态转移方程：
-1、不选择 nums[i]，如果在 [0, i - 1] 这个子区间内已经有一部分元素，使得它们的和为 j ，那么 dp[i][j] = true；
+1、不选择 nums[i]，如果在 [0, i - 1] 这个子区间内已经有一种选择方法的使得它们的和为 j ，那么 dp[i][j] = true；
 2、选择 nums[i]，如果在 [0, i - 1] 这个子区间内就得找到一部分元素，使得它们的和为 j - nums[i]。
-
 """
--- leetcode/DP/背包问题/494. 目标和(求方法数).py
+++ leetcode/DP/背包问题/494. 目标和(求方法数).py
@@ -1,4 +1,18 @@
+"""
 
+You are given an integer array nums and an integer target.
+
+You want to build an expression out of nums by adding one of the symbols '+' and '-' before each integer in nums and then concatenate all the integers.
+
+For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate them to build the expression "+2-1".
+Return the number of different expressions that you can build, which evaluates to target.
+
+Constraints:
+1 <= nums.length <= 20
+0 <= nums[i] <= 1000
+0 <= sum(nums[i]) <= 1000
+-1000 <= target <= 1000
+"""
 
 
 class Solution(object):
@@ -8,39 +22,37 @@ class Solution(object):
         :type S: int
         :rtype: int
         """
-        # sum_表示偏移量，因为有可能产生 j - nums[i]有可能是负数下标
-        sum_ = 0
-        for num in nums:
-            sum_ += num
 
-        # 假如说我们需要得到的和， 大于数组里所有元素的和，那么说明根本不存在一种符号能
-        # 得到这个结果
-        if abs(S) > abs(sum_):
-            return 0
+        offset = 1000
+        n = len(nums)
+        dp = [[0 for j in range(2001)] for i in range(n + 1)]
 
-        # 我们把dp的(0,0)中心坐标放在 sum 上， 这样使得 j + nums[i], j - nums[i]不会越界
-        total = sum_ * 2 + 1
-        dp = [[0 for j in range(total)] for i in range(len(nums))]
+        # dp[i][j] 表示使用nums前i个数字，能生成多少个总和为j的组合
+        # 考虑到 -1000 <= target <= 1000， 因为数组下标不能用负数来表示，所以我们把下标 0 -> 1000
+        # 所以原来 -1000 ~ 0 ~ 1000 => 0 ~ 1000 ~ 2000
+        # 所以dp[0][0] => dp[0][1000]
+        dp[0][offset] = 1
 
-        # 当nums[0] == 0 的时候，sum_ + nums[0] == sum_ - nums[0]
-        # 要是单纯附值的话那可能不行， 因为这个时候会省略附值一个1， 所以我们要对这种情况处理
-        if nums[0] == 0:
-            dp[0][sum_] = 2
-        else:
-            dp[0][sum_ + nums[0]] = 1
-            dp[0][sum_ - nums[0]] = 1
+        for i in range(1, n + 1):
+            for j in range(-1000, 1001):
+                # 因为目标数 j - nums[i-1] 有可能会是负数。所以我们要保证目标数是在这个范围以内的
+                if -1000 <= j - nums[i - 1] <= 1000:
+                    dp[i][offset + j] += dp[i - 1][offset + j - nums[i - 1]]
+                # 同样的，确定目标数不要越界
+                if -1000 <= j + nums[i - 1] <= 1000:
+                    dp[i][offset + j] += dp[i - 1][offset + j + nums[i - 1]]
 
-        for i in range(1, len(nums)):
-            for j in range(total):
-                # index out of range的数字我们按照0种可能性来进行处理
-                l = dp[i - 1][j - nums[i]] if 0 <= j - nums[i] < total else 0
-                r = dp[i - 1][j + nums[i]] if 0 <= j + nums[i] < total else 0
+        return dp[n][offset + S]
 
-                # 递推式子 dp[i][j] = dp[i-1][j - nums[i]] + dp[i-1][j + nums[i]]
-                dp[i][j] = l + r
-
-        return dp[-1][sum_ + S]
 
 """
-https://leetcode-cn.com/problems/target-sum/solution/dong-tai-gui-hua-si-kao-quan-guo-cheng-by-keepal/
+https://www.acwing.com/video/1899/
+dp[i][j] 表示使用nums前i个数字，能生成多少个总和为j的组合
+
+递推式
+dp[i][j] = dp[i-1][j-nums[i]] + dp[i-1][j+nums[i]]
+当前i个数组成总和为j的组合
+是有 前i-1个数，所组成总和为(j - nums[i])的数量
+和   前i-1个数，所组成总和为(j + nums[i])的数量
+之和构成的
 """
