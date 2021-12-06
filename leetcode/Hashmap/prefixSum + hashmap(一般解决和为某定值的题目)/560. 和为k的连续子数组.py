8758137db19c00ac30bd98f7dedb9af1943d972c
Yifu <yifu3@ualberta.ca>
2021-12-06 23:10:17 +0800

    560

--- leetcode/Hashmap/prefixSum + hashmap(一般解决和为某定值的题目)/560. 和为k的连续子数组.py
+++ leetcode/Hashmap/prefixSum + hashmap(一般解决和为某定值的题目)/560. 和为k的连续子数组.py
@@ -1,3 +1,18 @@
+"""
+Given an array of integers nums and an integer k, return the total number of continuous subarrays whose sum equals to k.
+
+
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+"""
+
 class Solution(object):
     def subarraySum(self, nums, k):
         """
@@ -6,36 +21,32 @@ class Solution(object):
         :rtype: int
         """
 
-
         prefixSum = {}
+        # 应对 nums=[3] k=3 这种情况
         prefixSum[0] = 1
-        Sum = 0
+        s = 0
         res = 0
 
         for i in range(len(nums)):
-            Sum += nums[i]
-            if (Sum - k) in prefixSum:
-                res += prefixSum[Sum - k]
+            s += nums[i]
+            # s - k = v
+            # s - v = k
+            # [0 ~ i] - [0 ~ j] = k
+            # [j ~ i] = k, 所以存在一个从[j ~ i]的连续数组和为k
+            if (s - k) in prefixSum:
+                res += prefixSum[s - k]
 
-            prefixSum[Sum] = prefixSum.get(Sum, 0) + 1
+            prefixSum[s] = prefixSum.get(s, 0) + 1
         return res
 
 """
 思路：https://www.youtube.com/watch?v=0rCaikfA7No
 代码：https://blog.csdn.net/fuxuemingzhu/article/details/82767119
 答案：
-1。prefixSum 就是遍历到i 的时候， sum(nums[:i+1])
+1. prefixSum 就是遍历到i 的时候， s(nums[:i+1])
 我们把每个prefixSum 都作为key, 把这个sum出现的次数作为value
 
-2。为什么要查 Sum - k 在不在prefixSum 的keys里?
-因为假如说 Sum - k 在prefixSum 里，就意味着存在一个j,使得 sum( nums[j...i]) 的和是k
-那么从 j - i,这个连续序列，就是我们想要找到的和为k的 连续序列
-
-3。为什么prefixSum[Sum] += 1?
+2. 为什么prefixSum[Sum] += 1?
 因为序列中有可能出现负数，例如 [2,3] 和 [2,3,-1,1], 他们的prefixSum都是相同的，但是最后
 的序列是不一样的。
-
-4。为什么要设置prefixSum[0] = 1
-因为在正常情况中，sum = 0是达不到（我们无法吧key = 0的情况加进字典），但是我们有可能会有一个字串sum - k = 0
-此时res 会要求把 key=0时value的数 取出来。所以我们要把它初始化为1
 """
