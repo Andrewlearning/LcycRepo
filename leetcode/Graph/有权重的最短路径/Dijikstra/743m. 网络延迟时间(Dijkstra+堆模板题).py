7e4ba661ee4938a38157e8eb52a9af8c9d8ba32b
Chen Yifu <yifu.chen@shopee.com>
2021-12-11 22:04:18 +0800

    add 787 743 dijikstra

---
+++ leetcode/Graph/有权重的最短路径/Dijikstra/743m. 网络延迟时间(Dijkstra+堆模板题).py
@@ -0,0 +1,59 @@
+"""
+有 n 个网络节点，标记为1到 n。
+
+给你一个列表times，表示信号经过 有向 边的传递时间。times[i] = (ui, vi, wi)，其中ui是源节点，vi是目标节点， wi是一个信号从源节点传递到目标节点的时间。
+
+现在，从某个节点K发出一个信号。需要多久才能使所有节点都收到信号？如果不能使所有节点收到信号，返回-1 。
+"""
+
+import heapq
+import collections
+
+
+class Solution(object):
+    def networkDelayTime(self, times, n, k):
+        """
+        :type times: List[List[int]]
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        # 初始化图，记录Key是每个点，value是可以到哪些目标点，以及到这些目标点的距离距离
+        graph = collections.defaultdict(list)
+        for f, to, weight in times:
+            graph[f].append((to, weight))
+
+        # pq 记录 从起始节点start 到当前节点 cur 的最短距离
+        # （start到当前节点最短距离，当前节点)。一开始初始化从最初起点出发，到自己节点的距离为0
+        # 把距离放在前面是因为heapq是根据第一个元素进行排序的
+        pq = [(0, k)]
+        #  从目标起点start出发到哪个节点: 最短距离。记录从初始节点到其他所有节点的最短距离
+        dist = {k: 0}
+        for to in range(1, n + 1):
+            if to != k:
+                dist[to] = float('inf')
+
+        while pq:
+            # 推出堆里从出发点到当前节点路径最短的点
+            # 因为当这个from点更新完所有的相邻点后，则不再遍历这个节点了
+            d1, cur = heapq.heappop(pq)
+
+            # 从当前节点，遍历当前节点所能到达的所有节点
+            for to, d2 in graph[cur]:
+                # 假如 起始节点到当前节点的距离 + 当前节点的距离到下一个节点的距离 < 起始节点到下一个节点的距离
+                # 那么更新 从起始节点到cur的下一个节点距离的最短路径值
+                # 起到剪枝的效果，如果没有的话也可以运行成功，但会超时
+                if d1 + d2 < dist[to]:
+                    dist[to] = d1 + time
+                    heapq.heappush(pq, (d1 + d2, to))
+
+        # 看看能不能遍历完整个图，遍历不完的话说明不能使所有节点收到信号
+        # 假如能遍历完的话，看到最远的节点需要多少时间，那就是需要多久才能使所有节点都收到信号
+        res = max(dist.values())
+        return res if res < float('inf') else -1
+"""
+time: E是times的长度，堆实现方式为O(ElogE),因为每个边都有可能被加进堆中
+space: O(N+E) 图的大小是O(E) 加上其他对象的大小O(N)
+"""
+# 链接：https://leetcode-cn.com/problems/network-delay-time/solution/wang-luo-yan-chi-shi-jian-by-leetcode/
+
---
+++ leetcode/Graph/有权重的最短路径/Dijikstra/787. K 站中转内最便宜的(Dijkstra+堆).py
@@ -0,0 +1,62 @@
+"""
+有 n 个城市通过一些航班连接。给你一个数组flights ，其中flights[i] = [from[i], to[i], price[i]] ，
+表示该航班都从城市 from[i] 开始，以价格 price[i] 抵达 to[i]。
+
+现在给定所有的城市和航班，以及出发城市 src 和目的地 dst，你的任务是找到出一条最多经过 k站中转的路线
+使得从 src 到 dst 的 价格最便宜 ，并返回该价格。 如果不存在这样的路线，则输出 -1。
+
+输入:
+n = 3, edges = [[0,1,100],[1,2,100],[0,2,500]]
+src = 0, dst = 2, k = 1
+输出: 200
+
+"""
+class Solution(object):
+    def findCheapestPrice(self, n, flights, src, dst, k):
+        """
+        :type n: int
+        :type flights: List[List[int]]
+        :type src: int
+        :type dst: int
+        :type k: int
+        :rtype: int
+        """
+
+        # key: 出发节点
+        # value: (到达节点，花销)
+        graph = collections.defaultdict(list)
+        for f, to, weight in flights:
+            graph[f].append((to, weight))
+
+        # (从起点到cur的花销，cur节点，从起点到cur节点的当前路径选择的转机次数)
+        pq = [(0, src, 0)]
+
+        # key: cur节点
+        # value: (从起点到cur节点的花销， 从起点到cur节点的当前路径选择的转机次数)
+        dist = {src: (0, 0)}
+        for to in range(n):
+            if to != src:
+                dist[to] = (float('inf'), float('inf'))
+
+        while pq:
+            d1, cur, times = heapq.heappop(pq)
+
+            # 假如已经遍历到了目标节点，且转机次数没有超过限制，则返回结果
+            if cur == dst and times <= k + 1:
+                return d1
+
+
+            for to, d2 in graph[cur]:
+                # 只有当同时满足 新花销>=原最少花销 且 新转机次数>=原最小花销的转机次数
+                # 才能跳过循环
+                if d1 + d2 >= dist[to][0] and times + 1 >= dist[to][1]:
+                    continue
+                dist[to] = (d1 + d2, times + 1)
+                heapq.heappush(pq, (d1 + d2, to, times + 1))
+
+        return -1
+
+"""
+本题相当于在743的基础上，加了一个转接距离这样一个限制，将这个限制假如进pq 和 dist中再加以判断就好
+https://leetcode.com/problems/cheapest-flights-within-k-stops/discuss/1477629/Python-DFS-and-Dijkstra-solutions-(beats-96)
+"""
