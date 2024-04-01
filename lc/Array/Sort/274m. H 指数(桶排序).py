1bfe2336e737a4cb047dc061d2c645c5d1b3b0ab
Yifu <yifu3@ualberta.ca>
2024-04-01 08:58:08 +0800

    update

--- lc/Array/Sort/274m. H 指数(桶排序).py
+++ lc/Array/Sort/274m. H 指数(桶排序).py
@@ -1,19 +1,47 @@
 """
-给定一位研究者论文被引用次数的数组（被引用次数是非负整数）。编写一个方法，计算出研究者的 h 指数。
+给定一位研究者论文被引用次数的数组（被引用次数是非负整数）。编写一个方法，计算出研究者的 h指数。
 
 h 指数的定义：h 代表“高引用次数”（high citations），一名科研人员的 h 指数是指他（她）的 （N 篇论文中）
-    总共有 h 篇论文分别被引用了至少 h 次。（其余的 N - h 篇论文每篇被引用次数 不超过 h 次。）
+    总共有 h 篇论文分别被引用了至少 h 次。（其余的N - h篇论文每篇被引用次数不超过 h 次。）
 
 例如：某人的 h 指数是 20，这表示他已发表的论文中，每篇被引用了至少 20 次的论文总共有 20 篇。
 
- 示例：
+示例：
 
 输入：citations = [3,0,6,1,5]
 输出：3
 解释：给定数组表示研究者总共有 5 篇论文，每篇论文相应的被引用了 3, 0, 6, 1, 5 次。
-     由于研究者有 3 篇论文每篇 至少 被引用了 3 次，其余两篇论文每篇被引用 不多于 3 次，所以她的 h 指数是 3。
+    由于研究者有 3 篇论文每篇 至少 被引用了 3 次，其余两篇论文每篇被引用 不多于 3 次，所以她的 h 指数是 3。
 """
 
+
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        # 把论文引用数从小到大排序，这样我们寻找h的时候可以从大往小找符合题目要求的
+        c = sorted(citations)
+        n = len(c)
+
+        for i in range(n):
+            # 要求，at least h papers that have each been cited at least h times.
+
+            # h最大数量是n，即作者发布了几篇论文
+            # h = n - i, 表示比i引用次数多的论文数量
+            h = n - i
+
+            # c[i] >= h，i是i ~ n-1篇论文中引用次数最少的，且i的论文引用次数 >= n-i(h)
+            # 那就说明存在有n-i(h)篇论文的引用次数 >= n - i(h)
+            if c[i] >= h:
+                return h
+
+        # 假如没找到，说明h不存在
+        return 0
+
+# Time O(nlogn), space O(1)
+# https://www.acwing.com/video/1652/
 
 class Solution(object):
     def hIndex(self, citations):
@@ -44,3 +72,4 @@ class Solution(object):
 
 # time On, Space On
 #  https://leetcode-cn.com/problems/h-index/solution/hzhi-shu-by-powcai/
+
--- lc/String/反转/151. 反转字符串里的单词I.py
+++ lc/String/反转/151. 反转字符串里的单词I.py
@@ -14,79 +14,57 @@ Explanation: Your reversed string should not contain leading or trailing spaces.
 
 
 class Solution(object):
-
     def reverseWords(self, s):
         """
         :type s: str
         :rtype: str
         """
-        if not s and len(s) == 0:
-            return s
+        # python中string是不可修改的，所以需要转换成list
+        s = list(s)
+        n = len(s)
+        
+        # i,j 对应原字符串单词的开头和结尾
+        # ni,nj 对应移位后字符串单词的开头和结尾
+        ni = i = 0
 
-        string = list(s)
+        while i < n:
+            # 跳过多余的空格，让i落到单词的第一个字幕上
+            if s[i] == " ":
+                i += 1
+                continue
+            
+            # 开始探索，并且让[j]的字母放在[nj]的位置上
+            j = i
+            nj = ni
+            while j < n and s[j] != " ":
+                s[nj] = s[j]
+                nj += 1
+                j += 1
+            
+            # 反转[ni ~ nj - 1]
+            self.reverse(s, ni, nj - 1)
 
-        # l放的是符合规范的单词和空格
-        l = 0
-        # r是探路者
-        r = 0
+            # 假如移动后的nj还没有越界，TODO
+            if nj < n:
+                s[nj] = ' '
 
-        # end是去除末尾空格后的最后一位
-        end = len(string) - 1
+            # TODO
+            ni = nj + 1
+            i = j
 
-        # 去除字符串末尾的空格
-        while end >= 0 and string[end] == " ":
-            end -= 1
+        # TODO
+        return ''.join(reversed(s[:ni - 1]))
 
-        # 全是空格的情况
-        if end == -1:
-            return ""
-
-        while r <= end:
-            start = l
-
-            # 让r走到单词的开头
-            while r <= end and string[r] == " ":
-                r += 1
-
-            # r在单词上走
-            while r <= end and string[r] != " ":
-                # 把r的元素，填到l的位置上去，这样能把字符串首部的空格去掉
-                string[l] = string[r]
-                l += 1
-                r += 1
-
-            # 把一个单词填完以后，我们把这个单词反转
-            self.reverse(start, l - 1, string)
-
-            # 假如说r还没走到尽头，且一个单词已经填完到l的位置上了，那么给l的位置上加一个空格
-            if r <= end:
-                string[l] = " "
-                l += 1
-
-        # 最后，我们把所有单词都反转了，并且空格只有一个，然后我们把整体都反转，得到答案
-        self.reverse(0, l - 1, string)
-
-        return "".join(string[:l])
-
-    def reverse(self, start, end, string):
-        while start < end:
-            string[start], string[end] = string[end], string[start]
-            start += 1
-            end -= 1
+    def reverse(self, s, l, r):
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
 
 """
-https://algocasts.io/episodes/BPp63kWr
 // Time: O(n), Space: O(n)
-答案：
-1.首先处理corner case
-2.创建一个string的list, p代表新string的指针，q代表旧string的指针
-他们两个都在string上遍历，替换是inplace的
-3.先把string后面的空格给去掉，用end --
-4.然后就可以开始while r <= end: 开始把开头的空格去掉，用q++
-5.当q到达正主的时候（单词），开始和p进行inplace赋值
-6.附值在q遇到空格是停止，那是单词中间的间隙，这时候要开始reverse新附值的单词了【start,l-1]
-7.把单词反转过后，加上空格，重复5-7的过程
-8.所有单词反转过之后，最后整体再反转一遍，得到结果
+https://www.acwing.com/video/1525/
+答案参考 https://www.acwing.com/activity/content/code/content/2188279/
 
 例子 "apple care"
      elppa  erac(先把每个单词反转一遍)
