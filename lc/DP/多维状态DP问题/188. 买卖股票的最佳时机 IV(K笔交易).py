46109e18a2c05fb4c1850d96260bbf695c6c88c0
Chen Yifu <yifu.chen@shopee.com>
2024-06-07 16:28:17 +0800

    update

--- lc/DP/多维状态DP问题/188. 买卖股票的最佳时机 IV(K笔交易).py
+++ lc/DP/多维状态DP问题/188. 买卖股票的最佳时机 IV(K笔交易).py
@@ -80,7 +80,19 @@ f[i + 1][j][0] = max(f[i][j][0], f[i][j][1] + prices[i])
 f[i + 1][j][1] = max(f[i][j][1], f[i][j-1][0] - prices[i])
 答案为 f[n][k+1][0]
 """
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        n = len(prices)
+        f = [[[-inf] * 2 for _ in range(k + 2)] for _ in range(n+1)]
 
+        for j in range(1, k+2):
+            f[0][j][0] = 0
+        for i, p in enumerate(prices):
+            for j in range(1, k+2):
+                f[i+1][j][0] = max(f[i][j][0], f[i][j-1][1] + p)
+                f[i+1][j][1] = max(f[i][j][1], f[i][j][0] - p)
+
+        return f[n][k+1][0]
 
 
 class Solution(object):
