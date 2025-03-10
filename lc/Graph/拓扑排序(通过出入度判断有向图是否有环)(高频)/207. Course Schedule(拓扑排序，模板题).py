7dc518b8280c1e0379bf9f7d93c72b38f4866075
Yifu <yifu3@ualberta.ca>
2025-03-10 23:50:31 +0800

    update

--- lc/Graph/拓扑排序(通过出入度判断有向图是否有环)(高频)/207. Course Schedule(拓扑排序，模板题).py
+++ lc/Graph/拓扑排序(通过出入度判断有向图是否有环)(高频)/207. Course Schedule(拓扑排序，模板题).py
@@ -1,25 +1,35 @@
 """
-记录可以完成所有课程的可行性路径
+你这个学期必须选修 num 门课程，记为0到num - 1 。
+
+在选修某些课程之前需要一些先修课程。
+先修课程按数组prereq 给出，其中prereq[i] = [ai, bi] ，
+表示如果要学习课程ai 则 必须 先学习课程 bi 。
+
+例如，先修课程对[0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。
+请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false
+
+输入: 2, [[1,0],[0,1]]
+输出: false
+解释:总共有 2 门课程。学习课程 1 之前，你需要先完成课程 0
+并且学习课程 0 之前，你还应先完成课程 1。这是不可能的。
 """
 class Solution(object):
-    def findOrder(self, num, prereq):
+    def canFinish(self, num, prereq):
         """
         :type num: int
         :type prereq: List[List[int]]
-        :rtype: List[int]
+        :rtype: bool
         """
-        from collections import defaultdict
-
         # 初始化图, 节点为 pre -> [next1, next2], 先修课 指向 后修课
         graph = defaultdict(list)
 
-        # 初始化入度，pre -> next -> 0
-        # pre 的入度为0，因为每节点指向pre
-        # next 入度为1，因为pre指向它
+        # 初始化入度,pre -> next -> 0
+        # pre 的入度为0,因为每节点指向pre
+        # next 入度为1,因为pre指向它
         indegree = defaultdict(int)
 
         for next, pre in prereq:
-            # 开始给图的顶点赋予意义，先修课->[下一节课]
+            # 开始给图的顶点赋予意义,先修课->[下一节课]
             graph[pre].append(next)
 
             # 构造入度表 node:指向这个node的节点数量有多少
@@ -34,12 +44,10 @@ class Solution(object):
 
         # count记录我们总共已经上过了几门课，假如有课不可到达，那么count是不可能 == numCourse的
         # 等于numcourse的
-        res = []
         count = 0
         while queue:
-            pre = queue.pop(0)
             count += 1
-            res.append(pre)
+            pre = queue.pop()
 
             # 删除上一个节点pre，所以与之对应pre节点的下一层节点所有的入度都要-1
             for next in graph[pre]:
@@ -49,11 +57,12 @@ class Solution(object):
                 if indegree[next] == 0:
                     queue.append(next)
 
-        if count == num:
-            return res
-        return []
+        return count == num
 
 """
 time(V+E) Space(V+E)
-需要记录每一次遍历到的结果
+1. 建立图
+2. 建立入度
+3. 找入口, 本题是从入度为0的开始
+4. BFS 拓扑排序
 """
---
+++ lc/Graph/拓扑排序(通过出入度判断有向图是否有环)(高频)/210. Course Schedule II(拓扑排序).py
@@ -0,0 +1,58 @@
+"""
+记录可以完成所有课程的可行性路径
+"""
+class Solution(object):
+    def findOrder(self, num, prereq):
+        """
+        :type num: int
+        :type prereq: List[List[int]]
+        :rtype: List[int]
+        """
+        from collections import defaultdict
+
+        # 初始化图, 节点为 pre -> [next1, next2], 先修课 指向 后修课
+        graph = defaultdict(list)
+
+        # 初始化入度,pre -> next -> 0
+        # pre 的入度为0,因为每节点指向pre
+        # next 入度为1,因为pre指向它
+        indegree = defaultdict(int)
+
+        for next, pre in prereq:
+            # 开始给图的顶点赋予意义,先修课->[下一节课]
+            graph[pre].append(next)
+
+            # 构造入度表 node:指向这个node的节点数量有多少
+            indegree[next] += 1
+
+
+        # 把入度为0的点都加入到queue里去
+        queue = []
+        for node in range(num):
+            if indegree[node] == 0:
+                queue.append(node)
+
+
+        res = []
+        while queue:
+            pre = queue.pop()
+            res.append(pre)
+
+            # 删除上一个节点pre,所以与之对应pre节点的下一层节点所有的入度都要-1
+            for next in graph[pre]:
+                indegree[next] -= 1
+
+                # 把入度为0的下一层节点加入到queue
+                if indegree[next] == 0:
+                    queue.append(next)
+
+        # 记录我们总共已经上过了几门课,假如有课不可到达, 例如[[1,0],[0,1]]这种
+        # 那么len(res)是不可能 = numCourse的, 说明没办法上完所有课，返回[]
+        if len(res) == num:
+            return res
+        return []
+
+"""
+time(V+E) Space(V+E)
+需要记录每一次遍历到的结果
+"""
